2ad2175abb2d1bafa68dbff5a5533bbb64d931b1
Vadim Chovgun <v.chovgun@armadalabs.com>
2019-01-14 12:55:11 +0300

    113331

--- js/js/script.js
+++ js/js/script.js
@@ -50,16 +50,16 @@ let mainList = {
     disount: false
 }
 function chooseGoods() {
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 5; i++) {
 
         let a = prompt("Какой тип товаров будем продавать?");
 
-        if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50 ) {
-            console.log('Всё верно!');
+        mainList.shopGoods[i] = a;
+        if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length <50 ) {
+            console.log ('Всё верно!');
             mainList.shopGoods[i] = a;
-        }else{
-            i = i - 1;
-         }
+        }
+       
     }
 }
 chooseGoods ();
